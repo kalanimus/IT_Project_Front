26c84526f66c0bb9bca2d1405980e00db97e9db1
Nikita Kalanchekaev <nikita.kalan@yandex.ru>
2025-05-29 15:31:34 +0300

    fixed imgs

--- front_kalm/my-app/src/pages/CabinetStudent/cabinetStudentPage.jsx
+++ front_kalm/my-app/src/pages/CabinetStudent/cabinetStudentPage.jsx
@@ -110,7 +110,7 @@ const CabinetStudentPage = () => {
           {/* ——— ЛЕВАЯ КОЛОНКА ——— */}
           <div className={classes.left}>
             <img
-              src="/avatars/StudentAvatarMock.png"
+              src={"/avatars/StudentAvatarMock.png"}
               alt={user.fullName}
               className={classes.avatar}
             />
